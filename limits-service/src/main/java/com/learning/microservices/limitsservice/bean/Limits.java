df24697af401c4d2ee53c22eecc27baa7610d4a0
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-11-15 21:34:24 -0500

    Creating Folder Structure and Hard Coded Limits Service

---
+++ limits-service/src/main/java/com/learning/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,32 @@
+package com.learning.microservices.limitsservice.bean;
+
+public class Limits {
+
+    private int minimum;
+    private int maximum;
+
+    public Limits() {
+        super();
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public Limits(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+}
