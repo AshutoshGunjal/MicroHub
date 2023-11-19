b03fe7f35862e0f11a3e14b2d23cddf683af91b0
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-11-18 22:35:31 -0500

    Hard Coded Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,54 @@
+package com.learning.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    //No Argument Constructor
+    public CurrencyExchange() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+}
