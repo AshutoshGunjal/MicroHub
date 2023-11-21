fec26a60c2935504a42b3071234cbf6f860a6ba4
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-11-20 19:19:01 -0500

    Configuring JPA and initializing the Data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'CAD','INR',60,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'USD','INR',80,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'EUR','INR',81,'');
